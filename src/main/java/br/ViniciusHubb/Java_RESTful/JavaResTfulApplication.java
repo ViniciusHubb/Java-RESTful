538d5e20515ee0dd48700a8e47d3feddb7b78d65
ViniciusOliveiraDev <viniciuscontato2024@gmail.com>
2025-03-20 00:18:56 -0300

    Project start

---
+++ src/main/java/br/ViniciusHubb/Java_RESTful/JavaResTfulApplication.java
@@ -0,0 +1,13 @@
+package br.ViniciusHubb.Java_RESTful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaResTfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaResTfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ViniciusHubb/Java_RESTful/JavaResTfulApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ViniciusHubb.Java_RESTful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaResTfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
